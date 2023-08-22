23ff39dd0a0cfad840b985005f38f3d8e8402e4c
Artem Kalinin <singlespreekrsk@gmail.com>
2023-08-22 23:12:54 +0300

    popupimg rdy

---
+++ scripts/Popup.js
@@ -0,0 +1,33 @@
+class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.querySelector('.popup__close-button').addEventListener('click', () => this.close());
+    this._popup.addEventListener('click', (event) => {
+      if (event.target === event.currentTarget) {
+        this.close()
+      }
+    });
+  }
+}
+
+export default Popup;
